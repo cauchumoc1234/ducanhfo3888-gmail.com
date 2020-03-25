9e0a9d7326467aeb1f52efdb14f856d4a7c234a3
Duc Anh <ducanhfo3888@gmail.com>
2020-03-25 19:40:23 +0700

    Cat run method

--- Cat.js
+++ Cat.js
@@ -2,9 +2,6 @@ var chalk = require('chalk');
 function Cat(name){
     this.name = name;
 }
-Cat.prototype.sayHi = function(){
-    console.log("Hi ! I am a cat. My name is " + chalk.blue(this.name));
-};
 Cat.prototype.run = function(){
     console.log("Running");
 }
