20c51dfb0bdcf9a20637bdb2bc9e5e4f4116a29c
Duc Anh <ducanhfo3888@gmail.com>
2020-03-25 18:37:18 +0700

    Cat run

--- Cat.js
+++ Cat.js
@@ -5,4 +5,7 @@ function Cat(name){
 Cat.prototype.sayHi = function(){
     console.log("Hi ! I am a cat. My name is " + chalk.blue(this.name));
 };
+Cat.prototype.run = function(){
+    console.log("Running");
+}
 module.exports = Cat;
