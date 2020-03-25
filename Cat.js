38c7e8848ca492cc59b3ce8d7901cbb18adb812d
Duc Anh <ducanhfo3888@gmail.com>
2020-03-25 19:41:21 +0700

    Delete 2 methods

--- Cat.js
+++ Cat.js
@@ -2,10 +2,4 @@ var chalk = require('chalk');
 function Cat(name){
     this.name = name;
 }
-Cat.prototype.sayHi = function(){
-    console.log("Hi ! I am a cat. My name is " + chalk.blue(this.name));
-};
-Cat.prototype.run = function(){
-    console.log("Running");
-}
 module.exports = Cat;
